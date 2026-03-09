01c26e49f20b1a7f6e7553cd59e0384d457452ff
Pedro Rolfsen Ittner <pedro@MacBook-Pro-de-Pedro-2372.local>
2026-03-09 11:03:14 -0300

    Hotfix: Import useEffect in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTransactions } from '../hooks/useTransactions';
 import { useAuth } from '../contexts/AuthContext';
 import { format, addMonths, subMonths } from 'date-fns';
